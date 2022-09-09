9f2a0861fb1e9794d58566bad4863e6722c72066
Hamna Siddiqui <hamnas200@gmail.com>
2022-09-09 11:14:22 -0700

    abs

---
+++ Survey_form/app.js
@@ -0,0 +1,7 @@
+let nextpage = document.getElementById("btn");
+// let colors = "red";
+let page = "./about.html";
+
+nextpage.addEventListener("click", function nextPage(){
+    window.location.href = page;
+})
