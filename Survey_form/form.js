89d76f34fb1af65e52bee80bd88c0de6c5f78a58
Hamna Siddiqui <hamnas200@gmail.com>
2022-09-14 10:16:59 -0700

    abc

--- Survey_form/form.js
+++ Survey_form/form.js
@@ -1,66 +1,18 @@
-function getAndUpdate(){
-    console.log("Your function is working");
-    fname = document.getElementById("fname").value;
-    lname = document.getElementById("lname").value;
-    email = document.getElementById("email").value;
-    if(localStorage.getItem("itemsJson") == null){
-        itemJsonarr = [];
-        itemJsonarr.push([fname, lname, email]);
-        localStorage.setItem("itemsJson", JSON.stringify(itemJsonarr));
+let fname = document.querySelector("#txt");
+// let lname = document.querySelector("#lname");
+// let email = document.querySelector("#email");
+const mybtn = document.querySelector(".btn-list");
+const list = document.querySelector(".container3 ul");
+
+mybtn.addEventListener('click',(e)=>{
+    if(fname.value != ""){
+        e.preventDefault();
     }
-    else{
-        //fetching
-        itemJsonarrStr = localStorage.getItem("itemsJson");
-        itemJsonarr = JSON.parse(itemJsonarrStr);
-        //again adding and saving in list
-        itemJsonarr.push([fname,lname, email]);
-        localStorage.setItem("itemsJson", JSON.stringify(itemJsonarr));
-    }
-    update();
-}
-function update(){
-    if(localStorage.getItem("itemsJson") == null){
-        itemJsonarr = []; 
-        localStorage.setItem("itemsJson", JSON.stringify(itemJsonarr));
-    }
-    else{
-        //fetching
-        itemJsonarrStr = localStorage.getItem("itemsJson");
-        itemJsonarr = JSON.parse(itemJsonarrStr);
-    }
-    //populating the table
-    tbody = document.getElementById("tablebody");
-    let str = "";
-    itemJsonarr.forEach((element, index) => {
-        str += `
-        <tr>
-        <th scope="row">${index+1}</th>
-        <td>${element[0]}</td>
-        <td>${element[1]}</td>
-        <td>${element[2]}</td>
-        <td><button class="btn btn-primary" onclick = "deleted(${index})">Delete</button></td>
-        </tr>`;
-    });
-    tbody.innerHTML = str;
-}
-let add = document.getElementById("add");
-add.addEventListener("click", getAndUpdate);
-update();
-function deleted(itemindex){
-    console.log("Delete",itemindex);
-    itemJsonarrStr = localStorage.getItem("itemsJson");
-    itemJsonarr = JSON.parse(itemJsonarrStr);
-    //Delete itemindex element from the array
-    itemJsonarr.splice(itemindex, 1);
-    localStorage.setItem("itemsJson", JSON.stringify(itemJsonarr));
-    update();
-}
-let clearing = document.getElementById("clear").value;
-function clearList(){
-    if(confirm("Do you really want to clear the List?")){
-    console.log("clearing the whole list");
-    localStorage.clear();
-    update();
-};
-    alert("Your to dos list has been cleared!");
-}
+    const myli = document.createElement("li");
+    myli.innerHTML = fname.value;
+    list.appendChild(myli);
+
+    const myspan = document.createElement("span");
+    myspan.innerHTML = "x";
+    list.appendChild(myspan);
+})
